5c2e859e99cebf12682d1d61af106247dbfff992
Jef Amirault <jef@amirault.org>
2020-05-24 17:34:48 -0400

    generate mandelbrot render based on z = # of iteraions, zoom, center point, and resolution

--- renderer.rb
+++ renderer.rb
@@ -1,32 +1,11 @@
 total_start = Time.now
-puts "start time: #{total_start}"
 
 require_relative 'complex_number'
 require 'oily_png'
 
 class Renderer
 
-  # no integers in MIN/MAX please!
-
-  # blob on left
-  # X_MIN = -1.810
-  # X_MAX = -1.7354
-  # Y_MIN = -0.021
-  # Y_MAX =  0.021
-
-  # origin
-  # X_MIN = -2.13 - 0.25
-  # X_MAX = 2.13 - 0.25
-  # Y_MIN = -1.2
-  # Y_MAX = 1.2
-
-  # domain 
-  X_MIN = -2.0
-  X_MAX = 2.0
-  Y_MIN = -2.0
-  Y_MAX = 2.0
-
-  PRECISION = 7
+  PRECISION = 17
 
   attr_accessor :bitmap, :points, :x_resolution, :y_resolution, :iterations, :x_min, :x_max, :y_min, :y_max, :x_offset, :y_offset, :iterations, :zoom
 
@@ -45,20 +24,20 @@ class Renderer
     y_range / y_resolution
   end
 
-  def initialize(iterations = 40, zoom = 1.0, center_x = 0, center_y = 0, width = 800, length = 800)
-    t0 = Time.now
-
+  def initialize(iterations = 40, zoom = 1.0, center_x = 0, center_y = 0, width = 960, length = 540)
     self.iterations = iterations
     self.x_resolution = width
     self.y_resolution = length
     self.x_offset = center_x
     self.y_offset = center_y
-    self.zoom = zoom
-    self.x_max = (width / 400.0) / zoom + center_x
-    self.x_min = (-1 * width / 400.0) / zoom + center_x
-    self.y_max = (length / 400.0) / zoom + center_y
-    self.y_min = (-1 * length / 400.0) / zoom + center_y
+    self.zoom = zoom * (y_resolution / 540) # scale the zoom with the resolution so you get the same bounds when zoom input is constant
+    self.x_max = (width / 400.0) / @zoom + center_x
+    self.x_min = (-1 * width / 400.0) / @zoom + center_x
+    self.y_max = (length / 400.0) / @zoom + center_y
+    self.y_min = (-1 * length / 400.0) / @zoom + center_y
 
+    t0 = Time.now
+    puts "Generating Mandelbrot Render #{t0}"
     x = @x_min
     @points = [] # [[a, b], ...]
     # puts "x_min: #{@x_min}"
@@ -71,13 +50,22 @@ class Renderer
       x += x_step
     end
     t1 = Time.now
-    puts "Resolution Mask created: #{x_resolution} x #{y_resolution} (#{t1 - t0} seconds)"
+    puts "Resolution Mask created in #{x_resolution} x #{y_resolution} (#{t1 - t0} seconds)"
     puts "Computing set membership for z = #{iterations}..."
-    @bitmap = @points.map do |p|
+    puts "0%... #{Time.now}"
+    last = @points.size - 1
+    @bitmap = @points.map.with_index do |p, i|
+      if i == last / 4
+        puts "25%... #{Time.now}"
+      elsif i == last/ 2
+        puts "50%... #{Time.now}"
+      elsif i == 3 * last / 4
+        puts "75%... #{Time.now}"
+      end
       [*p, ComplexNumber.new(p[0].round(PRECISION), p[1].round(PRECISION)).member?(iterations)]
     end
     t2 = Time.now
-    puts "Set membership computed (#{t2 - t1}) seconds"
+    puts "100%. Set membership computed in (#{t2 - t1}) seconds"
   end
 
   def export_png(filename = nil)
@@ -155,6 +143,7 @@ class Renderer
       colors << ChunkyPNG::Color.rgba(  0,  42, 255, 255)
       colors << ChunkyPNG::Color.rgba(  0,  21, 255, 255)
 
+    t0 = Time.now
     puts 'Applying transformations...'
     transform = @bitmap.map do |p|
       x = (p[0] - @x_min) * @x_resolution / x_range
@@ -168,7 +157,8 @@ class Renderer
     end
 
     png = ChunkyPNG::Image.new(@x_resolution, @y_resolution, ChunkyPNG::Color::TRANSPARENT)
-
+    t1 = Time.now
+    puts "Transformations completed in #{t1 - t0} seconds"
     puts 'Applying colors...'
     transform.each do |pixel|
       x = pixel[0].round
@@ -183,11 +173,15 @@ class Renderer
       png[x, y] = pixel[2]
     end
 
+    t2 = Time.now
+    puts "Colors added in #{t2 - t1} seconds"
     puts 'Saving...'
-    filename ||= "renders/#{Time.now.to_i}_center#{x_offset}_#{y_offset}_zoom#{zoom}_#{x_resolution}x#{y_resolution}_z#{iterations}.png"
+    filename ||= "renders/#{Time.now.to_i}_center#{x_offset}_#{y_offset}_zoom#{@zoom.round}_#{x_resolution}x#{y_resolution}_z#{iterations}.png"
     if png.save(filename, :interlace => true)
       puts "Exported to #{filename}"
     end
+    t3 = Time.now
+    puts "Rendered in #{t3 - t2} seconds"
   end
 end
 
@@ -199,18 +193,45 @@ end
 # #   f.puts "[0,0]]" 
 # # end
 
+# Origin (0 + 0i)
+# Left Blob (-1.765 + 0i)
+# Left Blob of the Left Blob (-1.786 + 0i)
+# Left Blob^3 (-1.78643 + 0i)
+# Starfish (-0.5622026215231 + 0.6428171490727i)
 
+# x = -0.5622026215231
+# y =  0.6428171490727
 
-z = 40
-zoom = 128
-# center
-x = -1.765
-y = 0
-# x_resolution, y_resolution = 960, 540
-x_resolution, y_resolution = 1920, 1080
+x = -0.5622026215231
+y =  0.6428171490727
+
+z = 120
+zoom = 2**24
+x_resolution, y_resolution = 960, 540
+# x_resolution, y_resolution = 1920, 1080
 # x_resolution, y_resolution = 3840, 2160
 # x_resolution, y_resolution = 7680, 4320
-# x_resolution, y_resolution = 15360, 8640
-r = Renderer.new(z, zoom, x, y, x_resolution, y_resolution)
 
-r.export_png
+# t0 = Time.now
+
+r = Renderer.new(z, zoom, x, y, x_resolution, y_resolution).export_png
+
+
+# # x_resolution, y_resolution = 1920, 1080
+# # x_resolution, y_resolution = 3840, 2160
+# # x_resolution, y_resolution = 7680, 4320
+# # r = Renderer.new(240, zoom, x, y, x_resolution, y_resolution).export_png
+
+
+# t1 = Time.now
+# puts "Total Time: #{t1 - t0} seconds"
+
+
+# x_resolution, y_resolution = 960, 540
+# x_resolution, y_resolution = 1920, 1080
+
+# Estimated 12 hours to complete:
+# (-10..400).each do |i|
+#   zoom = 2**(i/10.0)
+#   Renderer.new(z, zoom, x, y, x_resolution, y_resolution).export_png
+# end
